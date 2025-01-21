da3b08ba0e77c31778a89521e205164660a547de
BOT-MAKES162-crypto <Exsanaff@gmail.com>
2025-01-21 15:26:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=B1ggkQpC#77VFM3pi9-4S2FskDLT0swPNBBIJRKGidXOsNokS58U",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=9pg3AJYS#fcVoGwkF7rVJ1IP5JEOK2Gf59Ve2NKNhiHWxMjmmVg8",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5vp4rr.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*SEEN YOUR STATUS BY KAVI-EXE 🤍*",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*SEEN YOUR STATUS BY ᴇxㅤsᴀɴᴀㅤғғོꪾ 🤍*",
 ANTI_CALL: process.env.ANTI_CALL || "false",
 };
